9e76eee9677e5f87332f7f84d90587594400a8e4
BongUss <phatanhuyn@gmail.com>
2022-06-13 10:59:43 +0700

    modify build.xml to run test

--- test/com/khaphp/mathutil/test/core/MathUtilityTest.java
+++ test/com/khaphp/mathutil/test/core/MathUtilityTest.java
@@ -46,7 +46,7 @@ public class MathUtilityTest {
         Assert.assertEquals(6, MathUtil.getFactorial(3));  //tui mún 3! = 6
         Assert.assertEquals(24, MathUtil.getFactorial(4));  //tui mún 4! = 24
         Assert.assertEquals(120, MathUtil.getFactorial(5));  //tui mún 5! = 120
-        Assert.assertEquals(720, MathUtil.getFactorial(6));  //tui mún 6! = 720
+        Assert.assertEquals(120, MathUtil.getFactorial(6));  //tui mún 6! = 720
 
         //hàm giúp sosanh 2 vaue nào đó giống nhau hay ko
         //nếu giống nhau -> thày màu xanh đèn đường - đường thông, code ngon
